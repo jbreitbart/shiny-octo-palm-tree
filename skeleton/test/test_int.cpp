86f6c45bc9679d61df27772295c7b292efabd803
Jens Breitbart <jbreitbart@gmail.com>
2019-04-16 18:11:48 +0200

    Travis

--- skeleton/test/test_int.cpp
+++ skeleton/test/test_int.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
+#include "lib/add.hpp"
 
 TEST_CASE("Make integers great again")
 {
@@ -8,4 +9,5 @@ TEST_CASE("Make integers great again")
     auto n3 = 2;
     REQUIRE(n1 != n2);
     REQUIRE(n2 == n3);
+    REQUIRE(n1 + n2 == add(n1, n2));
 }
