85552e70e45ae5beaf9603fc33d9ec4e90df424b
Jens Breitbart <jbreitbart@gmail.com>
2019-04-16 15:43:40 +0200

    Travis

--- skeleton/src/main.cpp
+++ skeleton/src/main.cpp
@@ -1,13 +1,14 @@
 #include <chrono>
 #include <iostream>
 
-int main(int /*argc*/, char ** /*argv*/) {
-	auto start = std::chrono::high_resolution_clock::now();
+int main(int /*argc*/, char** /*argv*/)
+{
+    auto start = std::chrono::high_resolution_clock::now();
 
-	std::cout << "Hello world!" << std::endl;
+    std::cout << "Hello world!" << std::endl;
 
-	auto elapsed = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start).count();
-	std::cout << "Execution time was " << elapsed << " seconds.";
+    auto elapsed = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start).count();
+    std::cout << "Execution time was " << elapsed << " seconds.";
 
-	return 0;
+    return 0;
 }
--- skeleton/test/test_int.cpp
+++ skeleton/test/test_int.cpp
@@ -1,10 +1,11 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 
-TEST_CASE("Make integers great again") {
-	auto n1 = 1;
-	auto n2 = 2;
-	auto n3 = 2;
-	REQUIRE(n1 != n2);
-	REQUIRE(n2 == n3);
+TEST_CASE("Make integers great again")
+{
+    auto n1 = 1;
+    auto n2 = 2;
+    auto n3 = 2;
+    REQUIRE(n1 != n2);
+    REQUIRE(n2 == n3);
 }
