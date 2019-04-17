97f5c77248b53123917a0ddbc25c6f2e6ade8b66
Jens Breitbart <jbreitbart@gmail.com>
2019-04-17 14:07:13 +0200

    Travis

--- skeleton/test/test_add.cpp
+++ skeleton/test/test_add.cpp
@@ -6,8 +6,5 @@ TEST_CASE("Make integers great again")
 {
     auto n1 = 1;
     auto n2 = 2;
-    auto n3 = 2;
-    REQUIRE(n1 != n2);
-    REQUIRE(n2 == n3);
     REQUIRE(n1 + n2 == add(n1, n2));
 }
