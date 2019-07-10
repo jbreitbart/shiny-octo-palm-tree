1f964bff78f9bbfe9ca81c7d7424d274d0439518
Markus Heidemann <marheidemann@googlemail.com>
2019-07-10 22:11:14 +0200

    trying to get full test coverage

--- tasks/0_gcd_copy/skeleton/test/test_gcd_copy.cpp
+++ tasks/0_gcd_copy/skeleton/test/test_gcd_copy.cpp
@@ -3,4 +3,5 @@
 #include "lib/gcd.hpp"
 
 TEST_CASE("GCD") { REQUIRE(4 == gcd(4, 12)); }
+TEST_CASE("GCD_SWAPPED") { REQUIRE(4 == gcd(12, 4)); }
 TEST_CASE("GCD_NEGATIVE") { REQUIRE(-1 == gcd(-100, 25)); }
